74de31a72cd38e6f87c95e3a9946f2abca049e5d
Douglas <kekblob@gmail.com>
2023-06-06 12:56:03 +0100

    Create databaseModule.js

---
+++ src/customModules/databaseModule.js
@@ -0,0 +1,43 @@
+var axios = require("axios");
+
+var ags = [
+  {
+    filter: {
+      token: "hello",
+    },
+  },
+  {
+    documents: {
+      hello: "hello",
+    },
+  },
+];
+
+async function database(collection, action, args) {
+  // Guard clauses !
+
+  var config = {
+    method: "POST",
+    url: `${process.env.DATABASE_ENDPOINT}/action/${action}`,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key": process.env.DATABASE_API_KEY,
+    },
+    data: {
+      collection: collection,
+      database: "GreenGovanDatabase",
+      dataSource: "GreenGovanCluster",
+    },
+  };
+
+  args.forEach((element) => {
+    Object.assign(config.data, element);
+  });
+
+  console.log(config.data);
+}
+
+database("Bitch", "Fuck", ags);
+
+module.exports;
