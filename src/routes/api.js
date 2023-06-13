086dee30113173f4636c9c0ce1c909471c479886
JakeHallam1 <67105679+JakeHallam1@users.noreply.github.com>
2023-06-13 13:01:35 +0100

    fix

--- src/routes/api.js
+++ src/routes/api.js
@@ -12,7 +12,7 @@ router.use("/scanner", authMiddleware.authenticateAccessToken, scannerRoute);
 
 router.get("/", (req, res) => {
   var data = {
-    heading: "API Version 4",
+    heading: "API Version 5",
     route: "/api",
   };
   res.json(data);
