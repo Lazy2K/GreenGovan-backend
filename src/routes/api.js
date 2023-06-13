1d5fd21309a0b927b570ce3033523835efaf6b21
JakeHallam1 <67105679+JakeHallam1@users.noreply.github.com>
2023-06-13 14:58:22 +0100

    7

--- src/routes/api.js
+++ src/routes/api.js
@@ -12,7 +12,7 @@ router.use("/scanner", authMiddleware.authenticateAccessToken, scannerRoute);
 
 router.get("/", (req, res) => {
   var data = {
-    heading: "API Version 6",
+    heading: "API Version 7",
     route: "/api",
   };
   res.json(data);
